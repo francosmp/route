272e1d01a667c994cbdb76a7a4fb146b987c2d18
Franco Mp <stha-182@hotmail.com>
2019-10-16 05:00:39 -0500

    2019/10/16 05:00

--- ng.py
+++ ng.py
@@ -1,25 +1,28 @@
+import re
 import os
 import requests
 import threading
 import json
 import time
-import datetime
+
 
 def ng_rok():
-    os.system("sudo /bin/ngrok http 80")
+    os.system("ngrok http 80")
 
-def lampp():
-    os.system("sudo /opt/lampp/lampp start")
 
-tLampp = threading.Thread(target=lampp)
-tLampp.start()
+def xampp():
+    os.system("C:\\xampp\\xampp_start.exe")
 
-time.sleep(5)
+
+tXampp = threading.Thread(target=xampp)
+tXampp.start()
+
+time.sleep(10)
 
 tNgrok = threading.Thread(target=ng_rok)
 tNgrok.start()
 
-time.sleep(5)
+time.sleep(10)
 
 os.system("curl  http://localhost:4040/api/tunnels > tunnels.json")
 
@@ -29,36 +32,35 @@ with open('tunnels.json') as data_file:
 ngrok = datajson['tunnels'][1]['public_url']
 
 while 1:
-    time.sleep(1)
+    time.sleep(10)
 
-    status = 0
-
-    urlReact = 'http://434b5214.ngrok.io/inf-sec-java-ser/java/check'
+    urlReact = 'http://f145d809.ngrok.io/inf-sec-java-ser/java/check'
+    dataReact = {"url": "'" + ngrok + "'"}
 
     headers = {'Content-Type': 'application/json'}
-    urlEspejo = 'http://espejo.ngrok.io/inf-sec-php-ser/servicios-php.php'
-    dataEspejo = {"crud": "read", "index": "1", "codigo": "blanca"}
     params = {'sessionKey': '9ebbd0b25760557393a43064a92bae539d962103', 'format': 'xml', 'platformId': 1}
 
     try:
-        response = requests.post(urlEspejo, params=params, data=json.dumps(dataEspejo), headers=headers)
-        status = 200
+        response = requests.post(urlReact, params=params, data=json.dumps(dataReact), headers=headers)
+        urlEspejo = response.json()['url']
     except requests.exceptions.RequestException as e:
-        status = 404
+        print("React Apagado")
 
-    if status == 200 and response.status_code != 502 and response.status_code != 404:# Enviar post al servidor donde se encuentre el react e indicar el nuevo copia es el otro
+    with open('C:\\xampp\\htdocs\\inf-sec-php-ser\\servicios-php.php', 'r') as file:
+        data = file.read()
 
-        print("Prendido")
-        print(datetime.datetime.now())
+    m = re.search('[$]espejo = "(.*?)";', data)
 
-    if status == 404 or response.status_code == 502 or response.status_code == 404:# Enviar post al servidor donde se encuentre el react e indicar el nuevo root es este
+    if m:
+        urlPhp = m.group(0)
 
-        dataReact = {"url": ngrok + "/inf-sec-php-ser/servicios-php.php"}
+    if urlEspejo != "http://mirror.ngrok.io/inf-sec-php-ser/servicios-php.php":
+        data = data.replace(urlPhp, '$espejo = "' + urlEspejo + '";')
+        print(data)
+        print("if")
+    else:
+        data = data.replace(urlPhp, '$espejo = "";')
+        print("else")
 
-        try:
-            response = requests.post(urlReact, params=params, data=json.dumps(dataReact), headers=headers)
-        except requests.exceptions.RequestException as e:
-            print("Ya valió verga")
-
-        print("Apagado")
-        print(datetime.datetime.now())
+    f = open("C:\\xampp\\htdocs\\inf-sec-php-ser\\servicios-php.php", "w+")
+    f.write(data)
